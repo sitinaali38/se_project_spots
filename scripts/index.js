3f2b4f68d093fb0ccdb406c5d9f240332752bc0a
sitinaali <sitinaali38@gmaill.com>
2025-11-17 20:49:29 -0500

    add index.js and modifie index.html

---
+++ scripts/index.js
@@ -0,0 +1,26 @@
+const editProfileButton = document.querySelector(".profile__edit-button");
+const editProfileModal = document.querySelector("#edit-profile-modal");
+const editProfileCloseButton = editProfileModal.querySelector(
+  ".modal__close-button"
+);
+
+const newPostButton = document.querySelector(".profile__add-button");
+const newPostModal = document.querySelector("#new-post-modal");
+const newPostCloseButton = newPostModal.querySelector(".modal__close-button");
+
+editProfileButton.addEventListener("click", function () {
+  editProfileModal.classList.add("modal_is-opened");
+});
+
+// Close modal when clicking the close button
+editProfileCloseButton.addEventListener("click", function () {
+  editProfileModal.classList.remove("modal_is-opened");
+});
+
+newPostButton.addEventListener("click", function () {
+  newPostModal.classList.add("modal_is-opened");
+});
+
+newPostCloseButton.addEventListener("click", function () {
+  newPostModal.classList.remove("modal_is-opened");
+});
